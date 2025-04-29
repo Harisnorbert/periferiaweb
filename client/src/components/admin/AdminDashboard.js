a1a50bc618782cf38b82e45466d93259001af56f
Haris <haris.norbi00@gmail.com>
2025-04-29 13:20:43 +0200

    RendelesChart

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import RendelesChart from "RendelesChart";
+import RendelesChart from "./RendelesChart";
 
 const AdminDashboard = () => {
   return (
